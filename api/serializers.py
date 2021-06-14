b6119e5718c1344b50b3f38057635a6ac30aad49
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-14 18:36:57 +0300

    fix: correct the name of the field in serializers

--- api/serializers.py
+++ api/serializers.py
@@ -6,10 +6,10 @@ from .models import Poll, Vote
 class PollSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('pk', 'title', 'description', 'text')
-        models = Poll
+        model = Poll
 
 
 class VoteSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('poll_id', 'choice_id')
-        models = Vote
+        model = Vote
