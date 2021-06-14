722718332e361753feb4216b024b8d2d4f161b1d
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-14 14:41:42 +0300

    feat: create a Poll model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Poll(models.Model):
+    """Голосование с вариантами ответов."""
+    title = models.CharField(
+        verbose_name='Название голосования',
+        max_length=200,
+        help_text='Заполните название голосования',
+        unique=True
+    )
+    description = models.TextField(
+        verbose_name='Описание голосования',
+        blank=True,
+        help_text='Заполните описание голосования'
+    )
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='polls',
+        help_text='Организатор голосования выбирается автоматически',
+        verbose_name='Организатор голосования'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        help_text='Дата заполняется автоматически'
+    )
+    active = models.BooleanField(
+        verbose_name='Активность голосования',
+        default=True,
+        help_text='Выберите активно ли голосование'
+    )
+
+    def __str__(self):
+        return self.title
+
