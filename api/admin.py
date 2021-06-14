b44f4a17a631588a81bd260075c826052b02b2ef
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-14 15:45:25 +0300

    feat: register in the admin panel of the models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Poll, Vote
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display=('pk', 'poll_id', 'text')
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display=(
+        'pk', 'title', 'description', 'creator', 'pub_date', 'active'
+    )
+
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display=('pk', 'voter', 'poll_id', 'choice_id')
+
+
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Vote, VoteAdmin)
