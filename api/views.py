6ca1afb6d6dfe82b50b62d316a4d01141d84e3ac
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-14 19:56:14 +0300

    fix: add exception handling when saving to APIVote

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from copy import error
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -23,7 +24,10 @@ class APIVote(APIView):
     def post(self, request):
         serializer = VoteSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(voter=self.request.user)
+            try:
+                serializer.save(voter=self.request.user)
+            except Exception as error:
+                return Response({'message': f'Ошибка - {error}'})
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
