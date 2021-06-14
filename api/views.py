7f5b51b193c1c9fab12a9ed24ddf29e05a69b4fe
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-14 17:42:10 +0300

    feat: create a view APIPoll

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Poll, Vote
+from .serializers import PollSerializer, VoteSerializer
+
+
+class APIPoll(APIView):
+    """Создание голосования с вариантами ответов."""
+    def post(self, request):
+        # many = True?
+        serializer = PollSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
