985ccd604690ab3ca75ab91b0457e5f3cd33db48
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-14 18:06:11 +0300

    feat: create a views APIGetResult

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -25,3 +26,14 @@ class APIVote(APIView):
             serializer.save(voter=self.request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class APIGetResult(APIView):
+    """Получение результата по конкретному голосованию."""
+    def post(self, request):
+        poll_id = request.data['poll_id']
+        poll = get_object_or_404(Poll, id=poll_id)
+        poll_result = poll.votes
+        # many = True?
+        serializer = GetResultSerializer(poll_result)
+        return Response(serializer.data)
