3cdbf633436f25026ee7e77058c29cb5b71d19d0
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-15 22:33:08 +0300

    feat: add serializers for voting results

--- api/serializers.py
+++ api/serializers.py
@@ -8,13 +8,14 @@ class CreateChoiceSerializer(serializers.ModelSerializer):
         fields = ('id', 'text')
         model = Choice
 
+
 class PollSerializer(serializers.ModelSerializer):
     choices = CreateChoiceSerializer(many=True)
 
     class Meta:
         fields = ('id', 'title', 'description', 'choices')
         model = Poll
-    
+
     def create(self, validated_data):
         choices = validated_data.pop('choices')
         poll = Poll.objects.create(**validated_data)
@@ -28,3 +29,26 @@ class VoteSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('poll_id', 'choice_id')
         model = Vote
+
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    votes_count = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ('id', 'text', 'votes_count')
+        model = Choice
+
+    def get_votes_count(self, obj):
+        return obj.votes.count()
+
+
+class GetResultSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(read_only=True, many=True)
+    total_votes = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ('id', 'title', 'description', 'total_votes', 'choices')
+        model = Poll
+
+    def get_total_votes(self, obj):
+        return obj.votes.count()
