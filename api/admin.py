8550c1c2c661b816ca3a41fb1b6c048ef3c0400c
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-15 22:17:55 +0300

    style: add spaces

--- api/admin.py
+++ api/admin.py
@@ -4,17 +4,17 @@ from .models import Choice, Poll, Vote
 
 
 class ChoiceAdmin(admin.ModelAdmin):
-    list_display=('pk', 'poll_id', 'text')
+    list_display = ('pk', 'poll_id', 'text')
 
 
 class PollAdmin(admin.ModelAdmin):
-    list_display=(
+    list_display = (
         'pk', 'title', 'description', 'creator', 'pub_date', 'active'
     )
 
 
 class VoteAdmin(admin.ModelAdmin):
-    list_display=('pk', 'voter', 'poll_id', 'choice_id')
+    list_display = ('pk', 'voter', 'poll_id', 'choice_id')
 
 
 admin.site.register(Choice, ChoiceAdmin)
