da8c06b073de16c0e153ca607cba2f600be84cc3
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-17 10:02:22 +0300

    refactor: add name to urls

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import APIGetResult, APIPoll, APIVote
 
 urlpatterns = [
-    path('v1/createPoll/', APIPoll.as_view()),
-    path('v1/poll/', APIVote.as_view()),
-    path('v1/getResult/', APIGetResult.as_view()),
+    path('v1/createPoll/', APIPoll.as_view(), name='create-poll'),
+    path('v1/poll/', APIVote.as_view(), name='poll'),
+    path('v1/getResult/', APIGetResult.as_view(), name='get-result-poll'),
 ]
