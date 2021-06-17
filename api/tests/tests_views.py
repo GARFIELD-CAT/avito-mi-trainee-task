1bab05d5c554739be595ed9c8e78f166675fd34c
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-17 18:34:51 +0300

    test: add tests for APIVote

--- api/tests/tests_views.py
+++ api/tests/tests_views.py
@@ -89,3 +89,81 @@ class APIPollTests(APITestCase):
         self.assertEqual(
             response_bad_choices.status_code, status.HTTP_400_BAD_REQUEST
         )
+
+
+class APIVoteTests(APITestCase):
+    def setUp(self) -> None:
+        """Подготовка прогона теста. Вызывается перед каждым тестом."""
+        # Создаем тестового юзера.
+        self.user = User.objects.create(
+            username='test_user', password='Testpass12'
+        )
+        # Создаем токен для тестового юзера.
+        self.token = Token.objects.create(user=self.user)
+        # Создаем тестовый api клиент.
+        self.token_client = APIClient()
+        # Авторизуем клиент с помощью токена.
+        self.token_client.credentials(HTTP_AUTHORIZATION=f'Token {self.token}')
+        # Создаем тестовое голосование с вариантами ответов.
+        self.poll = Poll.objects.create(
+            title='Тестовое голосование', creator=self.user)
+        self.choice_1 = Choice.objects.create(
+            poll_id=self.poll, text='Вариант_1'
+        )
+        self.choice_2 = Choice.objects.create(
+            poll_id=self.poll, text='Вариант_2'
+        )
+
+    def test_create_vote(self):
+        """Убедитесь, что мы можем создать новый объект vote."""
+        url = reverse('poll')
+        data = {
+            'poll_id': self.poll.id,
+            'choice_id': self.choice_1.id
+        }
+
+        # Отправляем POST запрос.
+        response = self.token_client.post(url, data, format='json')
+
+        # Проводим проверку ответа.
+        # Статус код должен быть равен 201.
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Должен создаться 1 объект Vote.
+        self.assertEqual(Vote.objects.count(), 1)
+        # Должен создаться 1 объект Vote c указанным poll_id.
+        self.assertEqual(
+            Vote.objects.get().poll_id.title, 'Тестовое голосование'
+        )
+        # Должен создаться 1 объект Vote c указанным choice_id.
+        self.assertEqual(
+            Vote.objects.get().choice_id.text, 'Вариант_1'
+        )
+
+    def test_not_create_vote_with_bad_data(self):
+        """Убедитесь, что мы не можем создать некорректный объект vote."""
+        url = reverse('poll')
+        data_bad_poll_id = {
+            'poll_id': '',
+            'choice_id': self.choice_1.id
+        }
+        data_bad_choice_id = {
+            'poll_id': self.poll.id,
+            'choice_id': ''
+        }
+
+        # Отправляем POST запрос.
+        response_bad_poll_id = self.token_client.post(
+            url, data_bad_poll_id, format='json'
+        )
+        response_bad_choices_id = self.token_client.post(
+            url, data_bad_choice_id, format='json'
+        )
+
+        # Проводим проверку ответа.
+        # Статус код должен быть равен 400.
+        self.assertEqual(
+            response_bad_poll_id.status_code, status.HTTP_400_BAD_REQUEST
+        )
+        self.assertEqual(
+            response_bad_choices_id.status_code, status.HTTP_400_BAD_REQUEST
+        )
