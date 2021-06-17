bbca32069ee73dcccef1a76e15acb0c51f4a8dca
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-17 09:58:58 +0300

    feat: connect the postgresql database

--- service_api/settings.py
+++ service_api/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'service_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_poll_db',
+        'USER': 'postgres',
+        'PASSWORD': 'PTN4hssmc',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
