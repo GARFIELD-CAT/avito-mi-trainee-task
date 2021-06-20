86919cb4d1be8a1ba70306907d9b6abf166688ed
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-06-20 18:20:42 +0300

    refactor: delete the extra import

--- api/views.py
+++ api/views.py
@@ -2,7 +2,6 @@ from typing import Any
 from django.shortcuts import get_object_or_404
 
 from rest_framework import generics, status
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.serializers import BaseSerializer
 
